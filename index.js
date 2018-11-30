09263bfbee7b6ab9978aa5743e72b8844c8cdb64
Julius Tens <mail@juliustens.eu>
2018-11-30 02:19:11 +0100

    add logging

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require('express')
 const http = require('http')
 const corser = require('corser')
 const compression = require('compression')
+const morgan = require('morgan')
+const shorthash = require('shorthash').unique
+const fs = require('fs')
+const path = require('path')
 
 const link = require('./link')
 
@@ -14,6 +18,12 @@ const server = http.createServer(api)
 const allowed = corser.simpleRequestHeaders.concat(['User-Agent'])
 api.use(corser.create({requestHeaders: allowed})) // CORS
 api.use(compression())
+
+// setup the logger
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+morgan.token('id', (req, res) => req.headers['x-forwarded-for'] ? shorthash(req.headers['x-forwarded-for']) : shorthash(req.ip))
+api.use(morgan(':date[iso] :id :method :url :status :response-time ms', {stream: accessLogStream}))
 
 api.get('/', link)
 
