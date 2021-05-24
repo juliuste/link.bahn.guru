9c19b0f22b2ad6c3d8348191718d40dba8d56b1a
Julius Tens <mail@juliustens.eu>
2021-05-24 06:58:09 +0200

    make env.PORT required, log to stdout instead of file

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ const corser = require('corser')
 const compression = require('compression')
 const morgan = require('morgan')
 const shorthash = require('shorthash').unique
-const fs = require('fs')
-const path = require('path')
 
 const link = require('./link')
+
+const port = process.env.PORT
+if (!port) throw new Error('please provide a PORT environment variable')
 
 const api = express()
 const server = http.createServer(api)
@@ -19,10 +20,8 @@ api.use(corser.create({ requestHeaders: allowed })) // CORS
 api.use(compression())
 
 // setup the logger
-// create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 morgan.token('id', (req, res) => req.headers['x-forwarded-for'] ? shorthash(req.headers['x-forwarded-for']) : shorthash(req.ip))
-api.use(morgan(':date[iso] :id :method :url :status :response-time ms', { stream: accessLogStream }))
+api.use(morgan(':date[iso] :id :method :url :status :response-time ms', { stream: process.stdout }))
 
 api.get('/', link)
 
@@ -32,7 +31,6 @@ api.use((err, req, res, next) => {
 	next()
 })
 
-const port = process.env.PORT || 3000
 server.listen(port, (e) => {
 	if (e) return console.error(e)
 	console.log(`Listening on ${port}.`)
